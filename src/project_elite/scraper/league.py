5734beaa3817564f180eb951ab41928f37e9efa7
jziac <jan.ziacik@gmail.com>
2023-09-03 22:15:20 +0200

    3.9.2023 v2

--- src/project_elite/scraper/league.py
+++ src/project_elite/scraper/league.py
@@ -2,12 +2,16 @@ import scrapy
 import requests
 import re
 
+
 class LeagueScrapper():
 
-    paths = {"achievements": "//ul[@class='column-2']/li/a/text()",
-             "league_refs": "//a[@style='font-weight: 800;']/@href",
-             "long_name": "//h1[@class='plytitle text-center text-sm-left m-0']/text()"}
-    
+    PATHS = {
+        "achievements": "//ul[@class='column-2']/li/a/text()",
+        "league_refs": "//a[@style='font-weight: 800;']/@href",
+        "long_name": "//h1[@class='plytitle text-center text-sm-left m-0']"
+                     "/text()"
+    }
+
     def __init__(self, url):
         self.url = url
         self.html = requests.get(url).content
@@ -16,19 +20,23 @@ class LeagueScrapper():
     def get_u_id(self):
         u_id = re.findall("league\/(.+)", self.url)[0]
         return u_id
-    
+
     def get_long_name(self):
-        long_name = self.selector.xpath(LeagueScrapper.paths["long_name"]).getall()
+        long_name = self.selector.xpath(
+            LeagueScrapper.PATHS["long_name"]).getall()
         long_name = long_name[0].strip()
         return long_name
 
     def get_achievements(self):
-        achievements_list = self.selector.xpath(LeagueScrapper.paths["achievements"]).getall()
-        achievements_list = [achievement.strip() for achievement in achievements_list]
+        achievements_list = self.selector.xpath(
+            LeagueScrapper.PATHS["achievements"]).getall()
+        achievements_list = [achievement.strip()
+                             for achievement in achievements_list]
         return achievements_list
-    
+
     def get_season_data(self):
-        ref_list = self.selector.xpath(LeagueScrapper.paths["league_refs"]).getall()
+        ref_list = self.selector.xpath(
+            LeagueScrapper.PATHS["league_refs"]).getall()
         league_standings_dict = {}
         for link in ref_list:
             season_name = re.findall("standings/(.+)", link)[0]
@@ -36,7 +44,7 @@ class LeagueScrapper():
             season_dict = league_season_o.get_season_standings()
             league_standings_dict[season_name] = season_dict
         return league_standings_dict
-    
+
     def get_league_data(self):
         league_dict = {}
         league_dict["u_id"] = self.get_u_id()
@@ -44,16 +52,25 @@ class LeagueScrapper():
         league_dict["achievements_names"] = self.get_achievements()
         league_dict["season_tables"] = self.get_season_data()
         return league_dict
-    
+
+
 class LeagueSeasonScraper():
 
-    paths = {"table_section": "//table[@class = 'table standings table-sortable']//tbody[count(./tr/*)>1]",
-             "table_section_names": "//table[@class = 'table standings table-sortable']//tr[@class='title']/td//text()",
-            "table_row": "//table[@class = 'table standings table-sortable']//tr"}
-    header_names = ["position", "team", "gp", "w", "t", "l", "otw", "otl", "gf", "ga", "plus_minus", "tp", "postseason"]
+    PATHS = {
+        "table_section": "//table[@class = 'table standings table-sortable']"
+                         "//tbody[count(./tr/*)>1]",
+        "table_section_names": "//table[@class = 'table standings"
+                               " table-sortable']//tr[@class='title']"
+                               "/td//text()",
+        "table_row": "//table[@class = 'table standings"
+                          " table-sortable']//tr"
+        }
+    
+    header_names = ["position", "team", "gp", "w", "t", "l",
+                    "otw", "otl", "gf", "ga", "plus_minus", "tp", "postseason"]
 
     def __init__(self, url):
-        self.url = url 
+        self.url = url
         self.html = requests.get(self.url).content
         self.selector = scrapy.Selector(text=self.html)
 
@@ -62,31 +79,45 @@ class LeagueSeasonScraper():
         dict_section = {}
         for row_ind in range(1, n_rows + 1):
             dict_row = {}
-            one_row_path = path_section + "[" + str(row_ind) + "]" + "/td//text()"
-            one_row_html = path_section + "[" + str(row_ind) + "]" "/td[@class='team']//a/@href"
+            one_row_path = (path_section 
+                            + "[" 
+                            + str(row_ind) 
+                            + "]" 
+                            + "/td//text()")
+            one_row_html = (path_section 
+                            + "[" 
+                            + str(row_ind) 
+                            + "]" 
+                            + "/td[@class='team']//a/@href")
             row_data = self.selector.xpath(one_row_path).getall()
             url_team_season = self.selector.xpath(one_row_html).getall()[0]
-            url_team_general = re.findall("(.+)\/[0-9]{4}\-[0-9]{4}$", url_team_season)[0]
-            row_data = [value.strip() for value in row_data if value.strip()!=""]
+            url_team_general = re.findall(
+                "(.+)\/[0-9]{4}\-[0-9]{4}$", url_team_season)[0]
+            row_data = [value.strip()
+                        for value in row_data if value.strip() != ""]
             for ind in range(1, len(row_data)):
-                dict_row[LeagueSeasonScraper.header_names[ind]] = row_data[ind].strip()
+                dict_row[LeagueSeasonScraper.header_names[ind]
+                         ] = row_data[ind].strip()
             dict_row["url"] = url_team_general
             dict_section[row_data[0]] = dict_row
         return dict_section
-    
+
     def get_season_standings(self):
-        section_names = self.selector.xpath(LeagueSeasonScraper.paths["table_section_names"]).getall()
+        section_names = self.selector.xpath(
+            LeagueSeasonScraper.PATHS["table_section_names"]).getall()
         section_names = [name.strip() for name in section_names]
-        n_sections = len(self.selector.xpath(LeagueSeasonScraper.paths["table_section"]).getall())
+        n_sections = len(self.selector.xpath(
+            LeagueSeasonScraper.PATHS["table_section"]).getall())
         if n_sections > len(section_names):
             section_names = ["main"] + section_names
         dict_season = {}
         for part_ind in range(1, n_sections + 1):
-            path_section = LeagueSeasonScraper.paths["table_section"] + "[" + str(part_ind) + "]" + "/tr[not(@class = 'title')]"
-            dict_section = self.get_section_standings(path_section=path_section)
+            path_section = (LeagueSeasonScraper.PATHS["table_section"] 
+                            + "[" 
+                            + str(part_ind) 
+                            + "]" 
+                            + "/tr[not(@class = 'title')]")
+            dict_section = self.get_section_standings(
+                path_section=path_section)
             dict_season[section_names[part_ind - 1]] = dict_section
         return dict_season
-
-
-
-            
--- src/project_elite/scraper/player.py
+++ src/project_elite/scraper/player.py
@@ -2,6 +2,7 @@ import requests
 import scrapy
 import re
 
+
 class PlayerScraper:
 
     """Class for downloading information from individual players webpages;
@@ -15,7 +16,6 @@ class PlayerScraper:
     PLAYER_UID_REGEX = "player\/([0-9]+)\/"
 
     def __init__(self, url):
-
         """Arguments:
         url - url of player profile
         html - html code of player profile webpage
@@ -24,9 +24,8 @@ class PlayerScraper:
         self.url = url
         self.html = requests.get(self.url).content
         self.selector = scrapy.Selector(text=self.html)
-        
-    def get_info_all(self, years = None):
 
+    def get_info_all(self, years=None):
         """Wrapper function containing all individual methods for scrapping data into self.info_dict
             Output: dictionary used  for storing data on a player
                     Stats - keys: individual seasons -> (league - team) -> regular_season_playoff -> stats (list)
@@ -48,81 +47,78 @@ class PlayerScraper:
         dict_player["achievements"] = a_o.get_achievements()
         s_o = PlayerStats(selector=self.selector)
         dict_player["stats"] = s_o.get_all_stats(years=years)
-        dict_player["u_id"] = re.findall(PlayerScraper.PLAYER_UID_REGEX, 
+        dict_player["u_id"] = re.findall(PlayerScraper.PLAYER_UID_REGEX,
                                          self.url)[0]
         return dict_player
-    
+
+
 class PlayerGeneralInfo():
 
-        PATHS = {"player_name": "//h1[@class ="
-                                "'ep-entity-header__name']//text ()",
-                "general_info_left": "//div[contains(@class" 
-                                    "'ep-list__item--is-compact')]"
-                                    "/div[2][preceding-sibling::div[1]"
-                                    "[contains(text(), '",
-                 "general_info_right": "')]]//text()"
-                }
-        
-        INFO_NAMES = [
-            "Date of Birth", "Age", "Place of Birth", "Nation", "Position", "Height", "Weight", "Shoots", "Contract", "Cap Hit", "NHL Rights",
-            "Drafted", "Status", "Catches"
-            ]
-        KEEP_LIST = ["Drafted", "Nation"]
+    PATHS = {"player_name": "//h1[@class ="
+                            "'ep-entity-header__name']//text ()",
+             "gi_l": "//div[contains(@class,"
+                    "'ep-list__item--is-compact')]"
+                    "/div[2][preceding-sibling::div[1]"
+                    "[contains(text(), '",
+             "gi_r": "')]]//text()"
+    }
 
-        def __init__(self, selector):
+    INFO_NAMES = [
+        "Date of Birth", "Age", "Place of Birth", "Nation", "Position", "Height", "Weight", "Shoots", "Contract", "Cap Hit", "NHL Rights",
+        "Drafted", "Status", "Catches"
+    ]
 
-            """attribute: selector - selector created from html of whole player webpage"""
+    KEEP_LIST = ["Drafted", "Nation"]
 
-            self.selector = selector
+    def __init__(self, selector):
+        """attribute: selector - selector created from html of whole player webpage"""
 
-        def get_general_info(self):
-            dict_gi = self._get_info_wraper()
-            dict_gi["name"] = self._get_name()
-            f_r_o = FamilyRelations(selector=self.selector)
-            dict_gi["relations"] = f_r_o._get_relation_dict()
-            return dict_gi
+        self.selector = selector
 
-        def _get_name(self):
-            
-            """used to get player name"""
+    def get_general_info(self):
+        dict_gi = self._get_info_wraper()
+        dict_gi["name"] = self._get_name()
+        f_r_o = FamilyRelations(selector=self.selector)
+        dict_gi["relations"] = f_r_o._get_relation_dict()
+        return dict_gi
 
-            name = (self.selector
-                    .xpath(PlayerGeneralInfo.PATHS["player_name"])
-                    .getall())
-            name = [string.strip() for string in name]
-            name = [string for string in name if string != ""]
-            return name[0].strip()
-    
-        def _get_info_wraper(self):
+    def _get_name(self):
+        """used to get player name"""
 
-            """wraper method for downloading individual all info information from facts table"""
+        name = (self.selector
+                .xpath(PlayerGeneralInfo.PATHS["player_name"])
+                .getall())
+        name = [string.strip() for string in name]
+        name = [string for string in name if string != ""]
+        return name[0].strip()
 
-            dict_gi = {}
-            for info_name in PlayerGeneralInfo.INFO_NAMES:
-                keep_list = False
-                if info_name in PlayerGeneralInfo.KEEP_LIST:
-                    keep_list = True
-                dict_gi[info_name] = self._get_info(info_name=info_name, 
-                                                    keep_list=keep_list)
-            return dict_gi
+    def _get_info_wraper(self):
+        """wraper method for downloading individual all info information from facts table"""
 
-        def _get_info(self, info_name, keep_list):
+        dict_gi = {}
+        for info_name in PlayerGeneralInfo.INFO_NAMES:
+            keep_list = False
+            if info_name in PlayerGeneralInfo.KEEP_LIST:
+                keep_list = True
+            dict_gi[info_name] = self._get_info(info_name=info_name,
+                                                keep_list=keep_list)
+        return dict_gi
 
-            "method for getting one info value from Facts table on player's webpage"
+    def _get_info(self, info_name, keep_list):
+        "method for getting one info value from Facts table on player's webpage"
 
-            info_path_val = (PlayerGeneralInfo.paths["general_info_left"] 
-                            + info_name 
-                            + PlayerGeneralInfo.paths["general_info_right"])
-            info_val = self.selector.xpath(info_path_val).getall()
-            info_val = [string.strip() for string in info_val]
-            info_val = [string for string in info_val if string != ""]
-            if info_val == []:
-                info_val = [None]
-            if keep_list == False:
-                return info_val[0]
-            else:
-                return info_val
-            
+        info_path_val = (PlayerGeneralInfo.PATHS["gi_l"]
+                         + info_name
+                         + PlayerGeneralInfo.PATHS["gi_r"])
+        info_val = self.selector.xpath(info_path_val).getall()
+        info_val = [string.strip() for string in info_val]
+        info_val = [string for string in info_val if string != ""]
+        if info_val == []:
+            info_val = [None]
+        if keep_list == False:
+            return info_val[0]
+        else:
+            return info_val
 
 class FamilyRelations():
 
@@ -131,35 +127,34 @@ class FamilyRelations():
 
     PATHS = {
         "whole_text": "//p[@class='ep-text mb-2']"
-        }
-    
-    RELATION_REGEX = ("($|son|grandson|brother|father|uncle|" 
-                     "cousin|paternal\sgrandfather|maternal\sgrandfather|" "nephew|great\smaternal\sgrandfather|" "great\spaternal\sgrandfather|brother\-in\-law|" "brothers\-in\-law|son\-in\-law|sons\-in\-law|" "fathers\-in\-law|father\-in\-law|uncle\-in\-law|" "uncles\-in\-law|cousin\-in\-law|cousins\-in\-law|" "great\snephew|great\suncle)s{0,1}")
-    
-    def __init__(self, selector):    
+    }
+
+    RELATION_REGEX = ("($|son|grandson|brother|father|uncle|"
+                      "cousin|paternal\sgrandfather|maternal\sgrandfather|" "nephew|great\smaternal\sgrandfather|" "great\spaternal\sgrandfather|brother\-in\-law|" "brothers\-in\-law|son\-in\-law|sons\-in\-law|" "fathers\-in\-law|father\-in\-law|uncle\-in\-law|" "uncles\-in\-law|cousin\-in\-law|cousins\-in\-law|" "great\snephew|great\suncle)s{0,1}")
+
+    def __init__(self, selector):
         self.selector = selector
-        self._relations_regex = ("(" 
-                                 + FamilyRelations.RELATION_REGEX 
-                                 + ")" 
+        self._relations_regex = ("("
+                                 + FamilyRelations.RELATION_REGEX
+                                 + ")"
                                  + ":")
         self._relation_url_regex = ":(.+)(:|$)"
 
     def _get_individual_relations(self):
-
         """creates dictionary in which the keys are the relations that exist for the player and values are the html code in which u_ids of these relations can be found
         """
 
         text = (self.selector
-               .xpath(FamilyRelations.PATHS["whole_text"])
-               .getall())
+                .xpath(FamilyRelations.PATHS["whole_text"])
+                .getall())
         if text == []:
             return {}
         else:
             text = text[0]
         r_list = re.findall(self._relations_regex, text, flags=re.IGNORECASE)
         r_list = list(set(r_list))
-        data_list = re.findall(self._relation_url_regex, 
-                               text, 
+        data_list = re.findall(self._relation_url_regex,
+                               text,
                                flags=re.IGNORECASE)
         data_list = re.split(":", text, flags=re.IGNORECASE)
         data_list = [string_ for string_ in data_list if "a href" in string_]
@@ -167,31 +162,32 @@ class FamilyRelations():
         for ind in range(len(r_list)):
             dict_strings[r_list[ind][1]] = data_list[ind]
         return dict_strings
-    
+
     def _get_relation_dict(self):
         relations_uid = {}
         player_string_dict = self._get_individual_relations()
         for relation in player_string_dict:
-            list_uid = re.findall("player\=([0-9]+)", 
+            list_uid = re.findall("player\=([0-9]+)",
                                   player_string_dict[relation])
             if list_uid == []:
                 continue
             relations_uid[relation] = re.findall("\=([0-9]+)",
-                                                player_string_dict[relation])
-        return  relations_uid
-            
+                                                 player_string_dict[relation])
+        return relations_uid
+
+
 class PlayerStats():
 
     """class for downloading season data from stat tables on player webpage"""
 
-    #paths to access statistics in league and tournament tables on player webpage
+    # paths to access statistics in league and tournament tables on player webpage
     PATHS = {
         "path_league": "//div[@id='league-stats']",
         "path_tournament": "//div[@id='cup-stats']",
         "stats_table_l": "//table[contains(@class,'table table-"
                          "condensed table-sortable')]//tr[",
         "stats_table_r": "]/td",
-        "stat_years": "//table[contains(@class,'table table-condensed table-sortable')]/tbody/tr/@data-season", 
+        "stat_years": "//table[contains(@class,'table table-condensed table-sortable')]/tbody/tr/@data-season",
         "url_league": "[@class = 'league']/a/@href",
         "url_team": "[@class = 'team']//a[1]/@href",
         "stat_team": "[@class = 'team']/span/a[1]/text()",
@@ -202,13 +198,11 @@ class PlayerStats():
     }
 
     def __init__(self, selector):
-
         """attribute: selector - selector created from html of whole player webpage"""
 
         self.selector = selector
 
     def get_all_stats(self, years=None):
-
         """wrapper function for downloading stats from both league and tournament tables"""
 
         dict_stats = {}
@@ -216,9 +210,8 @@ class PlayerStats():
         dict_stats["tournaments"] = self._get_stats(years=years,
                                                     type="tournament")
         return dict_stats
-    
-    def _get_stats(self, type, years=None):
 
+    def _get_stats(self, type, years=None):
         """function for downloading data from the whole table (league, tournament) with the player season statistics
         """
 
@@ -226,8 +219,8 @@ class PlayerStats():
             path_type = PlayerStats.PATHS["path_league"]
         elif type == "tournament":
             path_type = PlayerStats.PATHS["path_tournament"]
-        dict_stats={}
-        path_years = path_type  + PlayerStats.PATHS["stat_years"]
+        dict_stats = {}
+        path_years = path_type + PlayerStats.PATHS["stat_years"]
         list_years = self.selector.xpath(path_years).getall()
         for ind in range(1, len(list_years) + 1):
             season = list_years[ind - 1]
@@ -235,21 +228,21 @@ class PlayerStats():
                 if list_years[ind - 1] not in years:
                     continue
             if season not in dict_stats:
-                dict_stats[season]={}
-            path_season = (path_type 
-                           + PlayerStats.PATHS["stats_table_l"] 
-                           + str(ind) 
+                dict_stats[season] = {}
+            path_season = (path_type
+                           + PlayerStats.PATHS["stats_table_l"]
+                           + str(ind)
                            + PlayerStats.PATHS["stats_table_r"])
             sub_dict = self._get_season_stats_from_table(path=path_season)
             for league in sub_dict:
                 if league in dict_stats[season]:
-                    dict_stats[season][league] = {**dict_stats[season][league], **sub_dict[league]}
+                    dict_stats[season][league] = {
+                        **dict_stats[season][league], **sub_dict[league]}
                 else:
                     dict_stats[season][league] = sub_dict[league]
         return dict_stats
-    
-    def _get_season_stats_from_table(self, path):
 
+    def _get_season_stats_from_table(self, path):
         """get data for one line in the table"""
 
         dict_season = {}
@@ -277,12 +270,12 @@ class PlayerStats():
         stat_playoff = [string.strip() for string in stat_playoff]
         stat_regular = self.selector.xpath(path_reg).getall()
         stat_regular = [string.strip() for string in stat_regular]
-        url_team = self._extract_general_url(path=path_url_team, 
+        url_team = self._extract_general_url(path=path_url_team,
                                              regex="(.+)\/[^\/]+$")
         if league_name is not None:
             url_league = self._extract_general_url(path=path_url_league,
-                                                    regex="(.+)\/stats")
-        else: 
+                                                   regex="(.+)\/stats")
+        else:
             url_league = None
         dict_season[league_name] = {}
         dict_season[league_name][team_name] = {}
@@ -294,14 +287,15 @@ class PlayerStats():
             leadership = leadership[0]
             dict_season[league_name][team_name]["leadership"] = leadership
         return dict_season
-    
+
     def _extract_general_url(self, path, regex):
         list_data = self.selector.xpath(path).getall()
         orig_url = list_data[0]
         url_list = re.findall(regex, orig_url)
         url = url_list[0]
         return url
-    
+
+
 class PlayerAchievements():
 
     PATHS = {
@@ -312,45 +306,21 @@ class PlayerAchievements():
 
     def __init__(self, selector):
         self.selector = selector
-        
-    def get_achievements(self, years=None):
 
+    def get_achievements(self, years=None):
         """method for downloading achievements of player into dictionary"""
 
         dict_achiev = {}
-        list_years = self.selector.xpath(PlayerAchievements.PATHS["achievements_years"]).getall()
+        list_years = self.selector.xpath(
+            PlayerAchievements.PATHS["achievements_years"]).getall()
         list_years = [string.strip() for string in list_years]
         for ind in range(1, len(list_years) + 1):
             if years is not None:
                 if list_years[ind - 1] not in years:
                     continue
-            path = (PlayerAchievements.PATHS["achievements_l"] 
-                    + str(ind) 
+            path = (PlayerAchievements.PATHS["achievements_l"]
+                    + str(ind)
                     + PlayerAchievements.PATHS["achievements_r"])
             award = self.selector.xpath(path).getall()
             dict_achiev[list_years[ind - 1]] = award
         return dict_achiev
-    
-
-
-
-
-
-
-    
-    
-
-
-    
-            
-            
-
-
-
-            
-
-
-
-
-        
-        
