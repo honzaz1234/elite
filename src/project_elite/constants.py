c355d3afacede7701a4190d5da8232a5a1a4a8d8
jziac <jan.ziacik@gmail.com>
2023-09-07 10:55:04 +0200

    adding constant file

---
+++ src/project_elite/constants.py
@@ -0,0 +1,23 @@
+
+GENERAL_INFO = "general_info"
+UID = "uid"
+
+#team dictionary keys
+STADIUM_INFO = "stadium_info"
+AFFILIATED_TEAMS = "affiliated_teams"
+RETIRED_NUMBERS = "retired_numbers"
+HISTORIC_NAMES = "titles"
+## general info
+PLAYS_IN = "plays_in"
+TEAM_COLOURS = "team_colours"
+PLACE = "place"
+YEAR_FOUNDED = "year_founded"
+LONG_NAME = "long_name"
+SHORT_NAME = "short_name"
+##stadium info
+ARENA_NAME = "arena_name"
+LOCATION = "location"
+CAPACITY = "capacity"
+CONSTRUCTION_YEAR = "construction_year"
+
+
