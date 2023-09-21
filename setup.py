c0f925da13aaabe56d0d14b726ad9084916ba67f
jziac <jan.ziacik@gmail.com>
2023-09-21 17:41:30 +0200

    setup of hockeydata change

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ version='0.1',
 description='Package for scraping data from eliteprospect website and transfering it in SQL database',
 url='https://github.com/honzaz1234/elite',
 author='jz',
-author_email='jan.ziacik@gmail.com',
+author_email='-',
 license='MIT',
 package_dir={"": "src"},
 packages = find_packages(
     where='src'
     ),
-install_requires=["sqlalchemy>=2.0", "scrapy", "requests"],
+install_requires=["sqlalchemy", "scrapy", "requests"],
 python_requires="~=3.10")
