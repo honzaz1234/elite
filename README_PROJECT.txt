8d2bcc6daa7322f9b4efc7371b363b6cfd0bac17
jziac <jan.ziacik@gmail.com>
2023-09-23 15:19:59 +0200

    final package set up

---
+++ README_PROJECT.txt
@@ -0,0 +1,5 @@
+Major contribution of this project is python package hockeydata whose purpose is to download hockey data from eliteprospects.com website into a DB. 
+This package is more closely described in README.md file. Additionaly, there can be also found jupyter notebook report in R programming language 
+made with the data downloaded with the methods from hockeydata package in the folder reports. Another jupyter notebook file in 
+folder tests then makes possible to test individual moduls found in the hockeydata package with various inputs. Lastly, the schema of database 
+which the methods from package create and put data into can be found in folder database. 
--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
-from setuptools import setup, find_packages
-setup(name='hockeydata',
-version='0.1',
-description='Package for scraping data from eliteprospect website and transfering it in SQL database',
-url='https://github.com/honzaz1234/elite',
-author='jz',
-author_email='-',
-license='MIT',
-package_dir={"": "src"},
-packages = find_packages(
-    where='src'
-    ),
-install_requires=["sqlalchemy", "scrapy", "requests"],
-python_requires="~=3.10")
+from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+
+long_description = (this_directory/"README.md").read_text()
+
+setup(
+    name='hockeydata',
+    version='0.1.0',
+    description='Package for scraping data from eliteprospect website and transfering it in SQL database',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/honzaz1234/elite',
+    author='jz',
+    author_email='-',
+    license='MIT',
+    license_files = ('LICENSE.txt',),
+    package_dir={"": "src"},
+    packages = ['hockeydata', 'hockeydata.tests'],
+    install_requires=["sqlalchemy", "scrapy", "requests"],
+    python_requires="~=3.10"
+)
