0d6db859ae4466fb7a8043a2edabe7f8667cc7f0
jziac <jan.ziacik@gmail.com>
2025-02-25 23:08:10 +0100

    fixes in join db class

--- src/gamedata/join_to_db.py
+++ src/gamedata/join_to_db.py
@@ -16,12 +16,12 @@ class DBJoiner():
             self, selected_seasons: list) -> dict:
         player_results = self.get_player_season_data(
             table=db.PlayerStats, selected_seasons=selected_seasons)
-        goalkeeper_results = self.get_all_player_season_data(
+        goalkeeper_results = self.get_player_season_data(
             table=db.GoalieStats, selected_seasons=selected_seasons)
         results = player_results + goalkeeper_results
         season_team_players: dict[str, dict[str, list[str]]] = {}
         for row in results:
-            season, team_name, player_name = row
+            player_id, player_name, team_id, team_name, season = row
 
             if season not in season_team_players:
                 season_team_players[season] = {}
@@ -29,30 +29,45 @@ class DBJoiner():
             if team_name not in season_team_players[season]:
                 season_team_players[season][team_name] = []
 
-            season_team_players[season][team_name].append(player_name)
+            season_team_players[season][team_name].append(
+                {"player_name": player_name, "player_id": player_id})
+            
         return season_team_players
     
     def get_player_season_data(
             self, table: Table, selected_seasons: list) -> list:
 
-        query = self.get_stats_query(
+        results = self.get_stats_query(
             table=table, selected_seasons=selected_seasons)
-        results = query.all()
+        # results = self.session.query.all()
         return results
     
     def get_stats_query(
-            self, table: Table, selected_seasons: list) -> TextClause:
+            self, table: Table, selected_seasons: list) -> list:
 
-        query = self.session.query(
+        query = (self.session.query(
             table.player_id,
             db.Player.name.label("player_name"),
             table.team_id,
             db.Team.team,
             db.Season.season,
-        ).join(db.Player, table.player_id == db.Player.id)\
-        .join(db.Team, table.team_id == db.Team.id)\
-        .join(db.Season, table.team_id == db.Season.id)\
+        ).join(db.Player, table.player_id == db.Player.id)
+        .join(db.Team, table.team_id == db.Team.id)
+        .join(db.Season, table.season_id == db.Season.id)
+        .join(db.League, table.league_id == db.League.id)
         .filter(db.Season.season.in_(selected_seasons))
+        .filter(db.League.uid == 'nhl')
+        .all())
+
         return query
+    
+class PlayerShiftUpdater():
+
+
+    def __init__(self):
+        pass
+
+
+    def 
 
 
