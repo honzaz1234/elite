2f6e9aae11fc25b04ef96246211eb0411fd65f40
jziac <jan.ziacik@gmail.com>
2025-03-03 04:46:31 +0100

    mapper from abbrevation to long name added

--- src/mappers/team_mappers.py
+++ src/mappers/team_mappers.py
@@ -97,6 +97,41 @@ NHL_FULL_NAME_UID_MAPPER = {
         'Montréal Wanderers': 3264
     }
 
+NHL_ABB_TEAM_NAME = {
+    "ANA": "Anaheim Ducks",
+    "ARI": "Arizona Coyotes",
+    "BOS": "Boston Bruins",
+    "BUF": "Buffalo Sabres",
+    "CGY": "Calgary Flames",
+    "CAR": "Carolina Hurricanes",
+    "CHI": "Chicago Blackhawks",
+    "COL": "Colorado Avalanche",
+    "CBJ": "Columbus Blue Jackets",
+    "DAL": "Dallas Stars",
+    "DET": "Detroit Red Wings",
+    "EDM": "Edmonton Oilers",
+    "FLA": "Florida Panthers",
+    "LAK": "Los Angeles Kings",
+    "MIN": "Minnesota Wild",
+    "MTL": "Montréal Canadiens",
+    "NSH": "Nashville Predators",
+    "NJD": "New Jersey Devils",
+    "NYI": "New York Islanders",
+    "NYR": "New York Rangers",
+    "OTT": "Ottawa Senators",
+    "PHI": "Philadelphia Flyers",
+    "PIT": "Pittsburgh Penguins",
+    "SJS": "San Jose Sharks",
+    "SEA": "Seattle Kraken",
+    "STL": "St. Louis Blues",
+    "TBL": "Tampa Bay Lightning",
+    "TOR": "Toronto Maple Leafs",
+    "VAN": "Vancouver Canucks",
+    "VGK": "Vegas Golden Knights",
+    "WSH": "Washington Capitals",
+    "WPG": "Winnipeg Jets"
+}
+
 
 def get_team_uid_from_abbrevation(team_abb: str) -> int:
         """returns NHL team uid as included in the DB based on the 3 letter"""
@@ -110,3 +145,10 @@ def get_team_uid_from_full_name(full_name: str) -> int:
         """ on team full name"""
         
         return NHL_FULL_NAME_UID_MAPPER[full_name]
+
+
+def get_nhl_full_name_from_abbrevation(team_abb: str) -> int:
+        """returns NHL team name DB based on the 3 letter"""
+        """team abbrevation"""
+
+        return NHL_ABB_TEAM_NAME[team_abb]
