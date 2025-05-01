edf60ba4a99b4d937fab46d888f9d477f4758cee
jziac <jan.ziacik@gmail.com>
2025-05-01 17:33:38 +0200

    new errors

--- src/errors.py
+++ src/errors.py
@@ -7,6 +7,7 @@ class EmptyReturnXpathValueError(ValueError):
                   )
         super().__init__(message)
 
+
 class WrongPlayDesc(AttributeError):
 
     def __init__(self, play_desc, play_type):
@@ -18,4 +19,39 @@ class WrongPlayDesc(AttributeError):
         super().__init__(message)
         self.play_desc = play_desc
         self.play_type = play_type
+
+
+class MissingPlayerID(KeyError):
+
+
+    def __init__(self, team_id, team_abb, player_number):
+        message = (
+            f"No player id found in the db_mapper for player from team"
+            f" {team_abb} ('team id': {team_id}) with number: {player_number}"
+                  )
+        super().__init__(message)
+
+
+class UpdateGameDataError(Exception):
+
+    def __init__(self, game_id, team_home, team_away, date):
+        message = (
+            f"Error in updating game data of game {game_id} between "
+            f"teams {team_home} and {team_away} "
+            f"from data {date}"
+                  )
+        super().__init__(message)
+
+
+class UpdatePlayKeyError(KeyError):
+
+
+
+    def __init__(self, play: dict):
+        message = {
+            f"Key missing in dict {play}"
+        }
+        super().__init__(message)
+
+
 
