31494c262a9e70e1826fd1e2f308ea5fe4915e43
jziac <jan.ziacik@gmail.com>
2025-08-20 21:19:01 +0200

    storage db creator updates

--- src/hockeydata/database_creator/storage_database_creator.py
+++ src/hockeydata/database_creator/storage_database_creator.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, LargeBinary, String, DateTime, ForeignKey
+from sqlalchemy import Boolean, Column, Integer, LargeBinary, String, DateTime, ForeignKey
 from sqlalchemy.orm import declarative_base
 from datetime import datetime
 
@@ -11,9 +11,9 @@ class Scrape(Base):
     __tablename__ = 'scrapes'
 
     id = Column(Integer, primary_key=True)
-    start_datetime = Column(DateTime, default=datetime.utcnow)
+    start_datetime = Column(DateTime, default=datetime.utcnow, nullable=False)
     end_datetime = Column(DateTime, nullable=True)
-    description = Column(String)
+    description = Column(String, nullable=False)
 
 
     def __init__(
@@ -35,32 +35,52 @@ class Player(Base):
     __tablename__ = 'players'
 
     id = Column(Integer, primary_key=True)
-    player_id = Column(Integer)
-    scrape_id = Column(Integer, ForeignKey('scraped.id'))
-    time = Column(DateTime, default=datetime.utcnow)
+    player_uid = Column(Integer, nullable=False)
+    scrape_id = Column(Integer, ForeignKey('scraped.id'), nullable=False)
+    is_goalie = Column(Boolean, nullable=False)
+    time = Column(DateTime, default=datetime.utcnow, nullable=False)
 
 
     def __init__(
-            self, player_id: int, scrape_id: int):
-        self.player_id = player_id
+            self, player_uid: int, scrape_id: int):
+        self.player_uid = player_uid
         self.scrape_id = scrape_id
         self.time =  datetime.utcnow()
 
 
     def __repr__(self):
-        return "<Player(id=%s, player_id='%s', scrape_id='%s', time=%s)>" % (
-            self.id, self.player_id, self.scrape_id, self.time
+        return "<Player(id=%s, player_uid='%s', scrape_id='%s', time=%s)>" % (
+            self.id, self.player_uid, self.scrape_id, self.time
         )
     
     
-class PlayerStats(Base):
+class SkaterStats(Base):
 
-    __tablename__ = 'player_stats'
+    __tablename__ = 'skater_stats'
 
 
     id = Column(Integer, primary_key=True)
     player_id = Column(Integer, ForeignKey('players.id'), nullable=False)
-    html_data = Column(LargeBinary) 
+    league_type = Column(String, nullable=False)
+    html_data = Column(LargeBinary, nullable=False) 
+
+
+    def __repr__(self):
+        return "<PlayerStats(id=%s, player_id=%s, html_data=%s)>" % (
+            self.id, self.player_id, self.html_data
+            )
+    
+
+class GoalieStats(Base):
+
+    __tablename__ = 'goalie_stats'
+
+
+    id = Column(Integer, primary_key=True)
+    player_id = Column(Integer, ForeignKey('players.id'), nullable=False)
+    league_type = Column(String, nullable=False)
+    season_type = Column(String, nullable=False)
+    html_data = Column(LargeBinary, nullable=False) 
 
 
     def __repr__(self):
@@ -69,9 +89,9 @@ class PlayerStats(Base):
             )
 
 
-class PlayerInfos(Base):
+class PlayerFacts(Base):
 
-    __tablename__ = 'player_infos'
+    __tablename__ = 'player_facts'
 
 
     id = Column(Integer, primary_key=True)
