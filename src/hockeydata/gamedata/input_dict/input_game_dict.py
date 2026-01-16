d9aa42e5657ff60d47bcefac32fadb49ec734c6c
Jan Ziacik <jan.ziacik@gmail.com>
2026-01-16 23:37:19 +0100

    dbmapperrefactor v2

--- src/hockeydata/gamedata/input_dict/input_game_dict.py
+++ src/hockeydata/gamedata/input_dict/input_game_dict.py
@@ -19,7 +19,7 @@ class InputEliteNHLmapper():
     def __init__(self, db_session: Session):
         self.db_method =  db_insert.DatabaseMethods(db_session)
         self.db_session = db_session
-        self.mappers_o = db_mapper.GetGameDBID(db_session)
+        self.mappers_o = db_mapper.GetGameDBMapper(db_session)
         self.update_on_conflict = False
         self.input_player_mapper_list = []
         self.input_stadium_mapper_list = []
@@ -165,7 +165,7 @@ class InputGameInfo():
             mappers: dict, update_on_conflict: bool, season: str
             ):
         self.db_session = db_session
-        self.mappers_o = db_mapper.GetGameDBID(db_session=self.db_session)
+        self.mappers_o = db_mapper.GetGameDBMapper(db_session=self.db_session)
         self.input_gi = InputGeneralInfo(
             db_session=self.db_session, 
             stadium_mapper=mappers["stadium"], update_on_conflict=update_on_conflict,
