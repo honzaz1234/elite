0498598c2bdc6d91188929321ff1216259bc9ea0
Jan Ziacik <jan.ziacik@gmail.com>
2026-01-19 01:40:12 +0100

    league scraper class added

--- src/hockeydata/entity_data/scrapers.py
+++ src/hockeydata/entity_data/scrapers.py
@@ -1,14 +1,15 @@
 import re
-import scrapy
 
 from abc import ABC, abstractmethod
+from datetime import datetime
 from playwright.sync_api import Page
+from scrapy import Selector
 from typing import Any
 
 import hockeydata.common_functions as cf
 import hockeydata.entity_data.playwright_setup.playwright_setup as ps
 
-from hockeydata.constants import PLAYER_UID_REGEX
+from hockeydata.constants import PLAYER_UID_REGEX, LEAGUE_UID_REGEX
 from hockeydata.logger.logging_config import logger
 
 
@@ -22,13 +23,14 @@ class PlaywrightScraper(ABC):
     @property
     @classmethod
     @abstractmethod
-    def CHECK_ARRIVAL_XPATH_KEY(cls):
+    def PATHS(cls):
         pass
+
 
     @property
     @classmethod
     @abstractmethod
-    def PATHS(cls):
+    def TYPE(cls):
         pass
 
 
@@ -48,7 +50,7 @@ class PlaywrightScraper(ABC):
     def go_to_page(self):
         ps.go_to_page_wait_selector(
             page=self.page, url=self.url,
-            sel_wait=self.PATHS[self.CHECK_ARRIVAL_XPATH_KEY]
+            sel_wait=self.PATHS["landing_check"]
             )
         ps.click_optional_button(
             page=self.page, sel_click=self.PATHS["accept_cookies"],
@@ -56,17 +58,24 @@ class PlaywrightScraper(ABC):
             )
 
 
-    def _scrape_data(self, xpath_name: str) -> str|None:
-        selector = scrapy.Selector(text=self.page.content())
+    def _scrape_data(
+            self, xpath_name: str, is_optional: bool = True) -> str|None:
+        selector = Selector(text=self.page.content())
         scraped_data = selector.xpath(
             self.PATHS[xpath_name]
             )  
         if not scraped_data:
-            self.scraped_data['missing_data'].append(xpath_name)
-            logger.info(
-                "Data type %s not present on the player page.", xpath_name
+            if is_optional:
+                message = (
+                    "Data type %s not present on the %s page.", 
+                    xpath_name,
+                    self.TYPE
                 )
-            return None
+                self.scraped_data['missing_data'].append(xpath_name)
+                logger.info(message)
+                return None
+            else:
+                cf.log_and_raise(message, ValueError)
         else:    
             logger.info("Data type %s succesfully scraped.", xpath_name) 
 
@@ -75,6 +84,30 @@ class PlaywrightScraper(ABC):
     
     @abstractmethod
     def get_data(self) -> dict:
+        pass
+
+
+class URLScraper(PlaywrightScraper):
+
+
+    CHECK_ARRIVAL_XPATH_KEY = ""
+    
+    
+    PATHS = {
+    }
+
+    def __init__(self, url: str, page: Page):
+        super().__init__(url=url, page=page)
+        self.scraped_data: dict[str, Any|None] = {
+            }
+
+
+    def get_data(self) -> dict:
+        pass
+
+
+    @abstractmethod
+    def _get_player_stats(self):
         pass
 
 
@@ -87,13 +120,11 @@ class PlayerScraper(PlaywrightScraper):
        c) player achievements 
     """
 
-
-    CHECK_ARRIVAL_XPATH_KEY = "player_facts"
-    
     
     PATHS = {
         "achievements": "//section[@id='career-highlights']",
         "accept_cookies": "//button[contains(., 'AGREE')]",
+        "landing_check": "//section[@id='player-facts']",
         "player_facts":  "//section[@id='player-facts']",
         "player_type": "//dt[contains(text(), 'Position')]"
                        "/following-sibling::dd/text()",
@@ -103,10 +134,12 @@ class PlayerScraper(PlaywrightScraper):
                            "and not(contains(., 'No Data Found'))]",
     }
 
+    TYPE = "player"
+
     def __init__(self, url: str, page: Page):
         super().__init__(url=url, page=page)
         self.scraped_data: dict[str, Any|None] = {
-                "player_uid": None,
+                "uid": None,
                 "player_type": None,
                 "player_facts": None,
                 "achievements": None,
@@ -120,20 +153,22 @@ class PlayerScraper(PlaywrightScraper):
             'Scraping of new player info at web adress: %s '
             'started', self.url
             )
-        self.scraped_data["player_uid"] = re.findall(
+        self.scraped_data["uid"] = re.findall(
             PLAYER_UID_REGEX, self.url)[0]
         self.scraped_data["player_type"] = cf.get_single_xpath_value(
-            sel=scrapy.Selector(text=self.page.content()),
+            sel=Selector(text=self.page.content()),
             xpath=self.PATHS["player_type"],
-            optional=False
+            is_optional=False
             )
         self.scraped_data["player_facts"] = self._scrape_data(
-            xpath_name="player_facts"
+            xpath_name="player_facts",
+            is_optional=False
             )
         self.scraped_data["achievements"] = self._scrape_data(
             xpath_name="achievements"
             )
         self.scraped_data['stats'] = self._get_player_stats()
+        self.scraped_data['time_scraped'] = datetime.now()
         logger.info(
             'Scraping of new player info at web adress: %s '
             'finished', self.url
@@ -257,6 +292,132 @@ class GoalieScraper(PlayerScraper):
             self.GOALIE_PATHS["title_check"], 
             timeout=10000
             )
+        
 
+class LeagueScraper(PlaywrightScraper):
 
         
+    PATHS = {
+        "achievements": "//header[./h2[contains(text(),'Awards')]]/"
+                        "following-sibling::div",
+        "accept_cookies": "//button[contains(., 'AGREE')]",
+        'first_year':"//li[last()]//a[contains(@class,'yearLink')]/text()",
+        "last_year": "//li[1]//a[contains(@class,'yearLink')]/text()",
+        "landing_check": "//h1/span[contains(@class,'LeagueHeader_titleMain')]",
+        "league_name":  "//h1/span[contains(@class,'LeagueHeader_titleMain')]",
+        "season": "//header[./h2[contains(text(),'Standings')]]"
+                        "/following-sibling::div[contains(@class,"
+                        "'Loader_loadingContentWrapper')  "
+                        "and not(contains(.,'No Data Found'))]",
+        "seasons": "//header[./h2[contains(text(),'Champions')]]/"
+                   "following-sibling::div",
+    }
+
+    TYPE = "league"
+
+
+    def __init__(self, url: str, page: Page):
+        super().__init__(url=url, page=page)
+        self.scraped_data: dict[str, Any|None] = {
+                "uid"
+                "league_name": None,
+                "achievements": None,
+                "seasons": None,
+                "stats": {},
+                "missing_data": [],
+                "season_range": {
+                    "first_season": None,
+                    "last_season": None
+                    }
+            }
+
+
+    def get_data(
+            self, scrape_seasons: bool = True, season_list: list = [] ) -> dict:
+        logger.info(
+            'Scraping of new league info at web adress: %s '
+            'started', self.url
+            )
+        self.scraped_data["uid"] = re.findall(
+            LEAGUE_UID_REGEX, self.url)[0]
+        self.scraped_data["league_name"] = self._scrape_data(
+            xpath_name="league_name",
+            is_optional=False
+            )
+        self.scraped_data["seasons"] = self._scrape_data(
+            xpath_name="seasons",
+            is_optional=False
+            )
+        self.scraped_data["achievements"] = self._scrape_data(
+            xpath_name="achievements"
+            )
+        self._get_list_of_years()
+        self._set_season_range()
+        if scrape_seasons:
+            self._get_stats(season_list=season_list)
+        self.scraped_data['time_scraped'] = datetime.now()
+        logger.info(
+            'Scraping of new player info at web adress: %s '
+            'finished', self.url
+            )
+        
+        return self.scraped_data
+
+
+    def _get_list_of_years(self) -> None:
+        sel = Selector(text=self.page.content())
+        first_year = self._get_year(sel=sel, xpath="first_year")
+        last_year = self._get_year(sel=sel, xpath="last_year")
+        self.year_list = [year for year in range(first_year, last_year + 1)]
+
+    
+    def _get_year(self, sel: Selector, xpath: str) -> int:
+        xpath = self.PATHS['seasons'] + self.PATHS[xpath]
+        extracted_year = sel.xpath(xpath).getall()[0]
+
+        return int(extracted_year)
+
+
+    def _set_season_range(self) -> None:
+        first_season = self._create_season_string(
+            year=self.year_list[0], 
+            preceeding=False
+            )
+        self.scraped_data['season_range']['first_season'] = first_season
+        last_season = self._create_season_string(
+            year=self.year_list[len(self.year_list) - 1], 
+            preceeding=False
+            )
+        self.scraped_data['season_range']['last_season'] = last_season
+
+
+    def _get_stats(self, season_list: list) -> None:
+        if season_list == []:
+            season_list = self.year_list
+        for year in season_list:
+            season = self._create_season_string(year=year, preceeding=False)
+            self.scraped_data['stats'][season] = self._get_year_stats(year=year)
+    
+
+    def _get_year_stats(self, year: str) -> Selector:
+        season_string = self._create_season_string(year=year, preceeding=True)
+        season_url = self.url + "/standings/" + season_string
+        ps.go_to_page_wait_selector(
+            page=self.page, 
+            url=season_url, 
+            sel_wait=self.PATHS["season"]
+            )
+
+        return self._scrape_data(xpath_name="season", is_optional=False)
+
+
+    def _create_season_string(self, year: str, preceeding: bool=True) -> list:
+        if preceeding == True:
+            year_plus = int(year) + 1
+            season_string = str(year) + "-" +  str(year_plus)
+        else:
+            year_minus = int(year) - 1
+            season_string = str(year_minus) + "-" + str(year)
+            
+        return season_string
+
