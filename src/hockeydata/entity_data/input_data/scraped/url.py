f228daba57c10d3207a05be1f757d4d2129ce388
Jan Ziacik <jan.ziacik@gmail.com>
2026-01-31 01:38:54 +0100

    url parsed data inputter added

--- src/hockeydata/entity_data/input_data/scraped/url.py
+++ src/hockeydata/entity_data/input_data/scraped/url.py
@@ -1,10 +1,11 @@
 from abc import abstractmethod
 from datetime import datetime
 from sqlalchemy.orm import Session
+from sqlalchemy.sql.schema import Table
 
 import hockeydata.database_creator.storage_database_creator as db
 
-from hockeydata.database_insert.db_insert import DatabaseMethods
+from hockeydata.database_insert.db_insert import StorageDatabaseMethods, Query
 from hockeydata.database_queries.database_query import StorageDBQuery
 from hockeydata.entity_data.input_data.scraped.base import HTMLInputter
 from hockeydata.logger.logging_config import logger
@@ -94,7 +95,7 @@ class PlayerTypeURLHTMLInputter():
         self.scrape_id = scrape_id
         self.season_id = season_id
         self.league_id = league_id
-        self.insert_db = DatabaseMethods(db_session=db_session)
+        self.insert_db = StorageDatabaseMethods(db_session=db_session)
 
 
     def input_data(self, scraped_data: list) -> None:
@@ -125,3 +126,80 @@ class GoalieURLHTMLInputter(PlayerTypeURLHTMLInputter):
 
 
     IS_GOALIE = True
+
+
+class URLInputter():
+
+
+    @property
+    @classmethod
+    @abstractmethod
+    def URL_TABLE(cls) -> Table:
+        pass
+
+    @property
+    @classmethod
+    @abstractmethod
+    def URL_TYPE(cls) -> str:
+        pass
+
+
+    def __init__(self, db_session: Session, 
+                 parsed_data: list[dict[str, int|list|str]]):
+        self.db_session = db_session
+        self.insert_db = StorageDatabaseMethods(db_session=db_session)
+        self.query = Query(db_session=db_session) 
+        self.parsed_data = parsed_data
+
+
+    def input_data(self) -> None:
+        for chunk in self.parsed_data:
+            self._input_page(chunk=chunk)
+
+
+    def _input_page(self, chunk: dict[str, int|list|str]) -> None:
+        insert_list = []
+        for url in chunk["urls"]:
+            url_entry = self._create_input_dict(url=url, chunk=chunk)
+            insert_list.append(url_entry)
+        self.insert_db.insert_update_or_ignore_on_conflict_bulk(
+            table=self.URL_TABLE, 
+            data=insert_list,
+            update=True
+            )
+
+
+    def _create_input_dict(
+            self, url: str, chunk: dict[str, int|list|str]
+            ) -> dict[str, int|str]:
+        
+        return {
+            "url": url,
+            "league_id": chunk["league_id"],
+            "season_id": chunk["season_id"],
+            "scrape_id": chunk["scrape_id"],
+            "is_goalie": chunk["is_goalie"]
+        }
+    
+
+    def input_parse_log(self, start_time: datetime, 
+                         end_time: datetime) -> None:
+        url_type_id = self.query._find_id_in_table(
+            table=db.URLType, 
+            url_type=self.URL_TYPE
+            )
+        self.insert_db._input_data(
+            table=db.URLLog, 
+            start_datetime=start_time,
+            end_datetime=end_time,
+            url_type_id=url_type_id
+            )
+        #maybe delete later?
+        self.db_session.commit()
+
+
+class PlayerURLInputter(URLInputter):
+
+
+    URL_TABLE = db.PlayerURL
+    URL_TYPE = "player"
