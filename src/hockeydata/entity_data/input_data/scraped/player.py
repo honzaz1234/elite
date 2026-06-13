35f6fe63357083841bbe66409b2deca0ed0bee33
Jan Ziacik <jan.ziacik@gmail.com>
2026-06-13 19:55:04 +0200

    player refactor data in method

--- src/hockeydata/entity_data/input_data/scraped/player.py
+++ src/hockeydata/entity_data/input_data/scraped/player.py
@@ -13,84 +13,83 @@ class PlayerHTMLInputter(HTMLEntityInputter):
 
 
     def __init__(
-            self, db_session: Session, scraped_data: dict):
+            self, db_session: Session):
         super().__init__(
-            db_session=db_session, 
-            scraped_data=scraped_data
+            db_session=db_session
             )
         self.is_goalie = None
 
 
-    def input_data(self) -> None:
-        self._set_is_goalie()
-        self._input_log()
-        self._input_player_facts_html()
-        self._input_achievements_html()
-        self._input_stats_htmls()
-        self._input_missing_data_logs()
+    def input_data(self, scraped_data) -> None:
+        self._set_is_goalie(scraped_data)
+        self._input_log(scraped_data)
+        self._input_player_facts_html(scraped_data)
+        self._input_achievements_html(scraped_data)
+        self._input_stats_htmls(scraped_data)
+        self._input_missing_data_logs(scraped_data)
         self.db_session.commit()
         logger.info(
             'Data for player %s succesfully inputed into storage DB.', 
-            self.scraped_data["uid"]
+            scraped_data["uid"]
             )
     
 
-    def _set_is_goalie(self) -> None:
+    def _set_is_goalie(self, scraped_data: dict) -> None:
 
         """method for establishing if the player is goalie or field player; important because of different structure of downloaded html
         """
 
-        position = self.scraped_data["player_type"]
+        position = scraped_data["player_type"]
         if position == "G":
             self.is_goalie =  True
         else:
             self.is_goalie = False
 
 
-    def _input_log(self):
+    def _input_log(self, scraped_data: dict):
         self.db_id = self.insert_db._input_data(
             table=db.PlayerLog, 
-            uid=self.scraped_data["uid"],
+            uid=scraped_data["uid"],
             is_goalie=self.is_goalie,
             scrape_id=self.scrape_id,
-            time_scraped=self.scraped_data['time_scraped']
+            time_scraped=scraped_data['time_scraped']
             )
 
 
-    def _input_player_facts_html(self) -> None:
+    def _input_player_facts_html(self, scraped_data: dict) -> None:
         self.insert_db._input_data(
             table=db.PlayerFacts, 
             player_id=self.db_id,
-            html_data=self.scraped_data["player_facts"]
+            html_data=scraped_data["player_facts"]
             )
         
 
-    def _input_achievements_html(self) -> None:
+    def _input_achievements_html(self, scraped_data: dict) -> None:
         self.insert_db._input_data(
             table=db.PlayerAchievements, 
             player_id=self.db_id,
-            html_data=self.scraped_data["achievements"]
+            html_data=scraped_data["achievements"]
             )
         
 
-    def _input_stats_htmls(self):
+    def _input_stats_htmls(self, scraped_data: dict):
         if self.is_goalie:
             stats_class = InputGoalieStatsHtml(
-                scraped_data=self.scraped_data['stats'], 
+                scraped_data=scraped_data['stats'], 
                 insert_db=self.insert_db,
                 player_id=self.db_id
                 ) 
         else:
             stats_class = InputSkaterStatsHtml(
-                scraped_data=self.scraped_data['stats'], 
+                scraped_data=scraped_data['stats'], 
                 insert_db=self.insert_db,
                 player_id=self.db_id
                 ) 
         stats_class._input_data()
 
 
-    def _input_missing_data_logs(self) -> None:
-        for data_type in self.scraped_data["missing_data"]:
+    def _input_missing_data_logs(self, scraped_data: dict) -> None:
+        for data_type in scraped_data["missing_data"]:
             self.insert_db._input_data(
                 db.PlayerMissingDataLog, 
                 player_id=self.db_id, 
